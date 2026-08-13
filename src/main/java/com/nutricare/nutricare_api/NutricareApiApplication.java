3572102f14cc0e8cca7216de7e20f9760e1874bd
Huy Nguyễn <huynguyen287123@gmail.com>
2026-08-13 16:31:40 +0700

    initialize project

---
+++ src/main/java/com/nutricare/nutricare_api/NutricareApiApplication.java
@@ -0,0 +1,14 @@
+package com.nutricare.nutricare_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricareApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NutricareApiApplication.class, args);
+	}
+
+}
